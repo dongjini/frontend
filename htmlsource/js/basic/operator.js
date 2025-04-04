45a21b600feaa1992bfe61fcb61cfdfde67ae082
joung dong jin <jdj5201@gmail.com>
2025-04-04 17:52:05 +0900

    자바스크립트 예제(변수, 연산, 자료형)

---
+++ htmlsource/js/basic/operator.js
@@ -0,0 +1,21 @@
+// 연산자
+// 산술, 비교, 연결
+let a = 4, b=5;
+
+console.log("a+b=", a+b)
+console.log("a-b=", a-b)
+console.log("a*b=", a*b)
+console.log("a/b=", a/b) // 0.8 / 자바 : 몫
+console.log("a%b=", a%b)
+
+
+// + : 연결연산자 숫자+문자
+let c = "3";
+console.log("a + c =", a + c)
+
+
+
+
+
+
+
